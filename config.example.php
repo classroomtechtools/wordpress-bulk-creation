ea565894376b997d5a9490cd8297777b85c1d00e
Anthony Kuske <anthonykuske@gmail.com>
2017-08-17 12:33:54 +0100

    Update example config

--- config.example.php
+++ config.example.php
@@ -5,6 +5,11 @@ return [
     'staffInputFile' => __DIR__.'/data/staff.csv',
     'elementaryScheduleInputFile' => __DIR__.'/data/elementary-schedule.csv',
 
+    'wordpressCliBin' => '/usr/bin/wp',
     'wordpressPath' => '/path/to/wordpress',
     'wordpressUrl' => 'http://www.mywordpresssite.com/',
+
+    'outputScript' => __DIR__.'/data/create-blogs.sh',
+    'outputIndexHtml' => __DIR__.'/data/blogs.html',
+    'outputIndexCsv' => __DIR__.'/data/blogs.csv',
 ];
